dade72fec335d31a77aa7b3b6e194cc4b08c0771
bung87 <crc32@qq.com>
2023-07-26 15:38:20 +0800

    init scroll

--- src/textee.nim
+++ src/textee.nim
@@ -5,6 +5,15 @@ const ProjectDir = currentSourcePath.parentDir
 let window = newWindow("textee", ivec2(1280, 800))
 window.size = (window.size.vec2 * window.contentScale).ivec2
 
+# var PIXEL_STEP  = 10
+var LINE_HEIGHT = 40
+# var PAGE_HEIGHT = window.size.y
+var scrollOffset = vec2(0, 0)
+var gOffsetY: float32
+
+window.onScroll = proc() =
+  # echo "onScroll ", window.scrollDelta
+  scrollOffset.y = window.scrollDelta.y * LINE_HEIGHT.float32
 makeContextCurrent(window)
 
 loadExtensions()
@@ -14,7 +23,6 @@ let bxy = newBoxy()
 var frame: int
 
 let typeface = readTypeface( ProjectDir / "assets/DejaVuSansMono.ttf")
-# let source = readFile(currentSourcePath)
 
 proc drawText(
   bxy: Boxy,
@@ -29,9 +37,8 @@ proc drawText(
   font.size = size
   font.paint = color
   let
-    arrangement = typeset(@[newSpan(text, font)], bounds = vec2(1280, 800))
+    arrangement = typeset(@[newSpan(text, font)], bounds = vec2(1280, 800) * 2)
     globalBounds = arrangement.computeBounds(transform).snapToPixels()
-  echo globalBounds
   let
     textImage = newImage(globalBounds.w.int, globalBounds.h.int)
     imageSpace = translate(-globalBounds.xy) * transform
@@ -50,17 +57,24 @@ window.onFrame = proc() =
 
   bxy.drawRect(rect(vec2(0, 0), window.size.vec2), BackgroundColor)
   var i = 0
+  var yOffset: float32
+  gOffsetY = gOffsetY + scrollOffset.y
   for line in currentSourcePath.lines:
+    if line.len == 0:
+      inc i
+      continue
+    yOffset = float32(FontSize * i) + scrollOffset.y
+
     bxy.drawText(
       $i,
-      translate(vec2(100, 100)),
+      translate(vec2(100, yOffset)),
       typeface,
       line,
       FontSize,
       TextColor
     )
     inc i
-
+  i = 0
   # End this frame, flushing the draw commands.
   bxy.endFrame()
   # Swap buffers displaying the new Boxy frame.
