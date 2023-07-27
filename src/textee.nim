d7311184c92d199ce7a31662c9536462c2fd1375
bung87 <crc32@qq.com>
2023-07-28 02:23:01 +0800

    render by line

--- src/textee.nim
+++ src/textee.nim
@@ -1,4 +1,4 @@
-import std/[os, monotimes, times]
+import std/[os, strutils]
 import boxy, opengl, windy
 import textboxes
 
@@ -18,7 +18,7 @@ let typeface = readTypeface( ProjectDir / "assets/DejaVuSansMono.ttf")
 
 const BackgroundColor = color(1, 1, 1, 1)
 const TextColor = color(0, 0, 0)
-const FontSize = 28
+const FontSize = 24
 
 var font = newFont(typeface)
 font.size = FontSize
@@ -28,51 +28,74 @@ var textBox = newTextBox(font, window.size.x, window.size.y)
 let source = readFile(currentSourcePath)
 textBox.text = source
 textBox.fontSize = FontSize
+textBox.lineHeight = FontSize
+
+var changed = true
 
 window.onScroll = proc() =
-  textBox.scrollBy(window.scrollDelta.y)
-  # textBox.adjustScroll()
+  # textBox.scrollBy(window.scrollDelta.y)
+  changed = true
 
-let transform = translate(vec2(100, 100))
-let
-  arrangement = textBox.layout(vec2(1280, 800) * 2)
-  globalBounds = arrangement.computeBounds(transform).snapToPixels()
-let
-  textImage = newImage(globalBounds.w.int, textBox.innerHeight)
-  imageSpace = translate(-globalBounds.xy) * transform
+window.onResize = proc () =
+  changed = true
 
-# textBox.setCursor(0)
-textImage.fillText(arrangement, imageSpace)
-let imageKey = "source"
-bxy.addImage(imageKey, textImage)
-var previousTime = default(MonoTime)
-var availablePeriod = 1000 div 60
+const Source = "source"
+let lines = splitLines(source)
+
+proc addAll() =
+  let transform = translate(vec2(100, 100))
+  let
+    arrangement = textBox.layout(vec2(1280, 800) * 2)
+    globalBounds = arrangement.computeBounds(transform).snapToPixels()
+  let
+    textImage = newImage(globalBounds.w.int, textBox.innerHeight)
+    imageSpace = translate(-globalBounds.xy) * transform
+
+  # textBox.setCursor(0)
+  textImage.fillText(arrangement, imageSpace)
+  
+  bxy.addImage(Source, textImage)
+
 # Called when it is time to draw a new frame.
 window.onFrame = proc() =
   # Clear the screen and begin a new frame.
-  bxy.beginFrame(window.size)
-  let thisTime = getMonoTime()
-  var framePeriod:int
-  if previousTime == default(MonoTime):
-     framePeriod = 0
+
+  if changed:
+    changed = false
+
+    bxy.beginFrame(window.size)
+
+    bxy.drawRect(rect(vec2(0, 0), window.size.vec2), BackgroundColor)
+
+    # bxy.drawImage(Source, vec2(0, textBox.scroll.y))
+    let transform = translate(vec2(0, 0))
+    let bounds = vec2(window.size.x.float32, textBox.lineHeight * 2)
+    
+    for i, line in lines:
+      let y = i.float32 * textBox.lineHeight * 1.6
+      let arrangement = typeset(@[newSpan(line, textBox.font)], bounds)
+      let globalBounds = arrangement.computeBounds(transform).snapToPixels()
+      let
+        textImage = newImage(bounds.x.int, bounds.y.int)
+        imageSpace = translate(-globalBounds.xy) * transform
+
+      textImage.fillText(arrangement, imageSpace)
+      
+      bxy.addImage($i, textImage)
+      let finalY = y + textBox.scroll.y
+      if finalY > window.size.y.float:
+        break
+      bxy.drawImage($i, vec2(0, finalY) )
+    # End this frame, flushing the draw commands.
+    bxy.endFrame()
+
+    # Swap buffers displaying the new Boxy frame.
+    window.swapBuffers()
+    inc frame
+
   else:
-    framePeriod = (thisTime - previousTime).inMilliseconds.int
-  
-  bxy.drawRect(rect(vec2(0, 0), window.size.vec2), BackgroundColor)
-  
-  bxy.drawImage(imageKey, vec2(0, textBox.scroll.y))
+    sleep(16)
 
-  # End this frame, flushing the draw commands.
-  bxy.endFrame()
-
-  # Swap buffers displaying the new Boxy frame.
-  window.swapBuffers()
-  inc frame
-  previousTime = thisTime
-  if framePeriod < availablePeriod:
-    let st = int(availablePeriod - framePeriod)
-    sleep(st)
-  
 
 while not window.closeRequested:
   pollEvents()
